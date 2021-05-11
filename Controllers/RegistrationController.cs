c4b1d66ce511ea129bb6b311e74d63c29cfe3e78
Kaustubh Suryawanshi <kmsuryawanshi456@hotmail.com>
2021-05-11 13:02:18 +0530

    Basic Structure

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineQuiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly ILogger<RegistrationController> _logger;
+        private readonly CoreDbContext _context;
+
+        public RegistrationController(ILogger<RegistrationController> logger, CoreDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Candidate> GetAll()
+        {
+            try
+            {
+                return _context.Candidate.ToList();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] Candidate candidate)
+        {
+            try
+            {
+                candidate.Active = true;
+                candidate.CreatedBy = candidate.Username;
+                candidate.CreatedDate = DateTime.Now;
+
+                _context.Candidate.Add(candidate);
+                _context.SaveChanges();
+
+                return new JsonResult("Candidate Added Successfully");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public JsonResult Put([FromBody] Candidate candidateNew)
+        {
+            try
+            {
+                var candidateOld = _context.Candidate.FirstOrDefault(x => x.CandidateId == candidateNew.CandidateId);
+
+                if (candidateOld != null)
+                {
+                    candidateOld.Username = candidateNew.Username;
+                    candidateOld.Password = candidateNew.Password;
+                    candidateOld.FirstName = candidateNew.FirstName;
+                    candidateOld.LastName = candidateNew.LastName;
+                    candidateOld.Address = candidateNew.Address;
+                    candidateOld.Email = candidateNew.Email;
+                    candidateOld.Mobile = candidateNew.Mobile;
+                    candidateOld.Birthday = candidateNew.Birthday;
+                    candidateOld.Gender = candidateNew.Gender;
+                    candidateOld.Photo = candidateNew.Photo;
+
+                    candidateOld.Active = true;
+                    candidateOld.ModifiedBy = candidateNew.Username;
+                    candidateOld.ModifiedDate = DateTime.Now;
+
+                    _context.Candidate.Update(candidateOld);
+                    _context.SaveChanges();
+
+                    return new JsonResult("Candidate Updated Successfully");
+                }
+
+                return new JsonResult("Please enter valid id");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                var candidate = _context.Candidate.FirstOrDefault(x => x.CandidateId == id);
+
+                if (candidate != null)
+                {
+                    _context.Candidate.Remove(candidate);
+                    _context.SaveChanges();
+
+                    return new JsonResult("Candidate Deleted Successfully");
+                }
+
+                return new JsonResult("Please enter valid id");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OnlineQuiz.Logging;
+using OnlineQuiz.Models;
+
+namespace OnlineQuiz
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+
+            services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(Configuration.GetConnectionString("Database")));
+            services.AddControllers();
+            services.AddSingleton<ILog, LogNLog>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILog logger)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.ConfigureExceptionHandler(logger);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
